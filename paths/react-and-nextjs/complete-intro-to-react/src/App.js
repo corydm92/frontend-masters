0d7a2ec0215728e670a4dc177a164d877160289d
Cory Morrissey <corydm92@gmail.com>
2025-12-05 09:42:52 -0800

    feat: added base code for react without dependencies

---
+++ paths/react-and-nextjs/complete-intro-to-react/src/App.js
@@ -0,0 +1,26 @@
+const Pizza = (props) => {
+	return React.createElement('div', {}, [
+		React.createElement('h1', {}, props.name),
+		React.createElement('p', {}, props.description),
+	]);
+};
+
+const App = () => {
+	return React.createElement('div', {}, [
+		React.createElement('h1', {}, "Padre Gino's"),
+		React.createElement(Pizza, {
+			name: 'Pepperoni',
+			description: 'Dope Pizza',
+		}),
+		React.createElement(Pizza, {
+			name: 'Americana',
+			description: 'Frenchfries and hotdogs',
+		}),
+	]);
+};
+
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(React.createElement(App));
