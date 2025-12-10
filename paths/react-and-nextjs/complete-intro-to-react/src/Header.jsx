719caef8241bd9502c962318b731e90f2bb292e9
Cory Morrissey <corydm92@gmail.com>
2025-12-10 09:24:47 -0800

    feat: finished react complete intro ecosystem section

--- paths/react-and-nextjs/complete-intro-to-react/src/Header.jsx
+++ paths/react-and-nextjs/complete-intro-to-react/src/Header.jsx
@@ -4,6 +4,7 @@ import { Link } from "@tanstack/react-router";
 
 export default function Header() {
     const [cart] = useContext(CartContext);
+
     return (
         <nav>
             <Link to="/">
