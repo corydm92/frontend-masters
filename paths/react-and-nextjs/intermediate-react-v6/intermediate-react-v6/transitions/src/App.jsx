ca1817ff2c00a5a20fbd6be7838d05c63d6ac8ba
Cory Morrissey <corydm92@gmail.com>
2025-12-29 11:33:55 -0800

    feat: understanding useTransition

--- paths/react-and-nextjs/intermediate-react-v6/intermediate-react-v6/transitions/src/App.jsx
+++ paths/react-and-nextjs/intermediate-react-v6/intermediate-react-v6/transitions/src/App.jsx
@@ -1,3 +1,65 @@
+import { useState, useEffect, useTransition } from 'react';
+import Score from './Score';
+import getScore from './getScore';
+
 export default function App() {
-  return <h1>Transitions</h1>;
+	const [isPending, startTransition] = useTransition();
+	const [game, setGame] = useState(1);
+	const [score, setScore] = useState({ home: '-', away: '-' });
+
+	async function getNewScore(game) {
+		setGame(game);
+		startTransition(async () => {
+			const newScore = await getScore(game);
+			startTransition(() => {
+				setScore((prev) => {
+					console.log(prev);
+					return newScore;
+				});
+			});
+		});
+	}
+
+	useEffect(() => {
+		console.log('useEffect');
+		getNewScore(game);
+	}, [game]);
+
+	const generateOptions = (number) => {
+		const arr = [];
+
+		for (let i = 1; i < number + 1; i++)
+			arr.push(
+				<option
+					value={i}
+					key={i}>
+					Game {i}
+				</option>
+			);
+
+		return arr;
+	};
+
+	return (
+		<div className='app'>
+			<h1>Game {game}</h1>
+			<select onChange={(e) => getNewScore(e.target.value)}>
+				{generateOptions(7)}
+			</select>
+			<div className={`loading-container ${isPending ? 'loading' : ''}`}>
+				<span className='spinner'>🏀</span>
+			</div>
+			<div>
+				<Score
+					isPending={isPending}
+					homeImage={score.homeImage}
+					homeName={score.homeName}
+					awayImage={score.awayImage}
+					awayName={score.awayName}
+					home={score.home}
+					away={score.away}
+				/>
+			</div>
+		</div>
+	);
 }
