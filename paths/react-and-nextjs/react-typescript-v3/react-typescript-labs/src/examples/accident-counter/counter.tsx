69979b9bf5b69b62043fb1679451f89a6a973134
Cory Morrissey <corydm92@gmail.com>
2026-01-28 09:04:00 -0800

    feat: moved thorugh typing form events exercise

--- paths/react-and-nextjs/react-typescript-v3/react-typescript-labs/src/examples/accident-counter/counter.tsx
+++ paths/react-and-nextjs/react-typescript-v3/react-typescript-labs/src/examples/accident-counter/counter.tsx
@@ -1,38 +1,65 @@
-import { useState } from 'react';
+import React, { useState, type Dispatch, type SetStateAction } from 'react';
 import { Card } from '$/common/components/card';
 import { Button } from './button';
+
+type CounterControlsProps = {
+  setCount: Dispatch<SetStateAction<number>>;
+};
+
+type CounterFormProps = {
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+};
+
+const CounterControls = ({ setCount }: CounterControlsProps) => {
+  return (
+    <div className="flex gap-2">
+      <Button onClick={() => setCount((prev) => prev - 1)}>➖ Decrement</Button>
+      <Button onClick={() => setCount(0)}>🔁 Reset</Button>
+      <Button onClick={() => setCount((prev) => prev + 1)}>➕ Increment</Button>
+    </div>
+  );
+};
+
+const CounterForm = ({ onSubmit }: CounterFormProps) => {
+  const [draftCount, setDraftCount] = useState(0);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setDraftCount(e.target.valueAsNumber);
+  };
+
+  return (
+    <form className="flex items-center gap-2" onSubmit={onSubmit}>
+      <input
+        className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
+        type="number"
+        name="input-count"
+        onChange={handleChange}
+        value={draftCount}
+      />
+
+      <Button>Update Counter</Button>
+    </form>
+  );
+};
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const handleDecrement = () => setCount((prev) => prev - 1);
-  const handleReset = () => setCount(0);
-  const handleIncrement = () => setCount((prev) => prev + 1);
+  const handleCounterFormSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const inputValue = Number(new FormData(e.currentTarget).get('input-count'));
+
+    setCount(inputValue);
+  };
 
   return (
     <Card className="border-primary-500 flex w-2/3 flex-col items-center gap-8">
       <h1>Days Since the Last Accident</h1>
       <p className="text-6xl">{count}</p>
-      <div className="flex gap-2">
-        <Button onClick={handleDecrement}>➖ Decrement</Button>
-        <Button onClick={handleReset}>🔁 Reset</Button>
-        <Button onClick={handleIncrement}>➕ Increment</Button>
-      </div>
-      <form
-        className="flex items-center gap-2"
-        onSubmit={(e) => {
-          e.preventDefault();
-          const input = new FormData(e.currentTarget).get('input-count');
-          setCount(Number(input));
-        }}
-      >
-        <input
-          className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
-          type="number"
-          name="input-count"
-        />
-        <Button>Update Counter</Button>
-      </form>
+
+      <CounterControls setCount={setCount} />
+      <CounterForm onSubmit={handleCounterFormSubmit} />
     </Card>
   );
 };
